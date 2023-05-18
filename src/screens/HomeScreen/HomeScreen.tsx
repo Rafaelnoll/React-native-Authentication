b319bba744e6c5a9ae4fa72b9b11b93248cf8032
Rafael Noll <rafanoll2004@gmail.com>
2023-05-18 11:22:52 -0300

    Feature: Sign out button in Home screen

--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -1,11 +1,28 @@
 /* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 import {View, Text} from 'react-native';
+import auth from '@react-native-firebase/auth';
 
 const HomeScreen = () => {
+  const onSignOutPressed = () => {
+    auth().signOut();
+  };
+
   return (
-    <View>
+    <View style={{flex: 1}}>
       <Text style={{fontSize: 24, alignSelf: 'center'}}>Home, sweet home</Text>
+      <Text
+        onPress={onSignOutPressed}
+        style={{
+          width: '100%',
+          textAlign: 'center',
+          color: 'red',
+          marginTop: 'auto',
+          marginVertical: 20,
+          fontSize: 20,
+        }}>
+        Sign Out
+      </Text>
     </View>
   );
 };
